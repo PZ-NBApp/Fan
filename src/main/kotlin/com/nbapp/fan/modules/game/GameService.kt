ec955b463ffbbb04f205dbccaef1cfaa67d405aa
JPogodzinski <pogodzinskij@gmail.com>
2020-06-04 13:01:46 +0200

    added jpa in build.gradle

--- src/main/kotlin/com/nbapp/fan/modules/game/GameService.kt
+++ src/main/kotlin/com/nbapp/fan/modules/game/GameService.kt
@@ -1,11 +1,11 @@
 package com.nbapp.fan.modules.game
 
-import com.nbapp.adminserver.modules.team.Team
-import com.nbapp.adminserver.modules.team.TeamRepository
+
+import com.nbapp.fan.modules.team.TeamRepository
 import org.springframework.stereotype.Service
 
 @Service
-class GameService(private val gameRepository: GameRepository, private val teamRepository:TeamRepository) {
+class GameService(private val gameRepository: GameRepository) {
     fun getGames():Iterable<Game>{
         return gameRepository.findAll()
     }
@@ -13,35 +13,5 @@ class GameService(private val gameRepository: GameRepository, private val teamRe
     fun getGameById(id : Int): Game {
         return gameRepository.getOne(id)
     }
-
-    fun addGame(game: Game): Game {
-        return gameRepository.save(game)
-    }
-
-    fun deleteGame(id : Int) {
-        return gameRepository.deleteById(id)
-    }
-
-    fun updateGame(id:Int, text:Map<String,Int>) {
-        val hostResult:Int=text.getValue("hostResult").toInt()
-        val guestResult:Int=text.getValue("guestResult").toInt()
-        val game:Game=gameRepository.getOne(id)
-        val hostId:Int=game.hostId
-        val guestId:Int=game.guestId
-        val host:Team= teamRepository.getOne(hostId)
-        val guest:Team=teamRepository.getOne(guestId)
-        game.updateHostResult(hostResult)
-        game.updateGuestResult(guestResult)
-        if (hostResult>guestResult){
-            host.wonGame()
-            guest.lostGame()
-
-        }
-        else
-        {
-            host.lostGame()
-            guest.wonGame()
-        }
-        gameRepository.save(game)
-    }
+    
 }
